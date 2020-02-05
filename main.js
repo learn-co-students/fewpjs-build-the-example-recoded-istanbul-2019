8f899cb9eb3a78931cf5e4dba2e77e6b22eab76d
Abdulrahman Y. idlbi <adlogi@gmail.com>
2020-02-05 17:16:15 +0300

    Done.

--- main.js
+++ main.js
@@ -5,8 +5,30 @@ const FULL_HEART = '♥'
 // Your JavaScript code goes here!
 
 
+document.addEventListener("DOMContentLoaded", function() {
+  
+  document.querySelector('#modal').classList.add('hidden');
 
+  for (const glyph of document.querySelectorAll('.like-glyph')) {
+    glyph.onclick = () => {
+      mimicServerCall()
+      .then(() => {
+        if (glyph.classList.contains('activated-heart')) {
+          glyph.classList.remove('activated-heart');
+        } else {
+          glyph.classList.add('activated-heart');
+        }
+      })
+      .catch((err) => {
+        document.querySelector('#modal').classList.remove('hidden');
+        document.querySelector('#modal-message').textContent = err.message;
+        setTimeout(function(){ document.querySelector('#modal').classList.add('hidden'); }, 5000);
+      })
+    };
+  }
 
+});
+done();
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
@@ -23,3 +45,4 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
