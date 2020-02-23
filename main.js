bb235ddb6d37754509ee3f51b95b7abdc7d7dbce
Mavera <mkaymakci.sm@gmail.com>
2020-02-23 09:11:38 +0300

    Done.

--- main.js
+++ main.js
@@ -1,20 +1,45 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = "♡";
+const FULL_HEART = "♥";
 
 // Your JavaScript code goes here!
-
-
-
+const likeHeartBtn = document.querySelectorAll(".like-glyph");
+const div = document.querySelector("div");
+let i = 0;
+likeHeartBtn.forEach(element => {
+  element.addEventListener("click", function() {
+    mimicServerCall()
+      .then(() => {})
+      .catch(function(error) {
+        div.classList.remove("hidden");
+        let p = document.createElement("p");
+        p.textContent = error;
+        div.appendChild(p);
+        document.body.append(error);
+        setTimeout(function() {
+          div.classList.add("hidden");
+        }, 5000);
+      })
+      .then(function() {
+        if (i == 1) {
+          element.classList.remove("activated-heart");
+          i = 0;
+        } else {
+          element.classList.add("activated-heart");
+          i = 1;
+        }
+      });
+  });
+});
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+      let isRandomFailure = Math.random() < 0.2;
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
