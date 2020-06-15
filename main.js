380919a682b52cb28c4e41a0c3ae2b11265d55b5
Maher Suleyman <maher.suleyman.ms@gmail.com>
2020-06-15 06:30:40 +0300

    Done.

--- main.js
+++ main.js
@@ -4,7 +4,69 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+  // Declaring constants to reference errors.
+  const errorModal = document.getElementById('modal')
+  const errorMessage = document.getElementById('modal-message')
 
+ function hideError() {
+   errorModal.classList.add('hidden')
+   errorMessage.textContent = ''
+ }
+
+ function showError(specificError) {
+   errorModal.classList.remove('hidden')
+   errorMessage.textContent = specificError
+ }
+
+ function likeListener() {
+   // An array of all of the hearts on the page.
+   const hearts = document.getElementsByClassName('like-glyph')
+
+   // Iterating over the array to have access to a listener for each like
+   for (heart of hearts) {
+     clickLike(heart)
+   }
+ }
+
+ function clickLike(like) {
+   // Adding event listener for each like.
+   like.addEventListener("click", () => {
+     // Server call is replacing a fetch in this example
+     mimicServerCall()
+     // if the request goes through we want to update the likes
+     .then((response) => {
+       updateLikes(like)
+     })
+     /*
+     If there is an error we want to display it for 5 seconds.
+     We can confirm that this works by seeing if a heart is added
+     when the error message is displayed. Given that our hearts do
+     not currently change unless there is no error message, we can
+     assume that this is working as intended.
+     */
+     .catch((error) => {
+       showError(error);
+       /*
+          setTimeout function requires that we reference the function
+          we want to use rather than calling it directly.
+       */
+       setTimeout(hideError, 5000);
+     })
+   })
+ }
+
+ function updateLikes(specificLike) {
+  // Previous if statement is not necessary as toggle has same functionality
+  specificLike.classList.toggle('activated-heart')
+ }
+
+ // Here is our main function where we hide the errors and then create our listener
+ function main() {
+   hideError()
+   likeListener()
+ }
+
+ main()
 
 
 //------------------------------------------------------------------------------
